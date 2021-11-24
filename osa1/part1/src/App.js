1d9e2c737ae139961412438afdbb8ce55f1ffc70
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-11-24 22:10:32 +0200

    1.4

--- osa1/part1/src/App.js
+++ osa1/part1/src/App.js
@@ -2,30 +2,26 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Course course={course}/>
-
-      <Parts part1={part1}
-             part2={part2}
-             part3={part3}/>
-
-      <Total exercises1={part1.exercises}
-             exercises2={part2.exercises}
-             exercises3={part3.exercises}/>
+      <Parts parts= {parts}/>
+      <Total parts={parts}/>
     </div>
   )}
 
@@ -43,15 +39,13 @@ const App = () => {
   const Parts = (props) => {
     return(
       <>
-        <Part part={props.part1}/>
-        <Part part={props.part2}/>
-        <Part part={props.part3}/>
+        {props.parts.map(p => <Part part={p}/>)}
       </>
     )
   }
 
   const Total = (props) => {
-    return(<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>)
+    return(<p>Number of exercises {props.parts.map(p => p.exercises).reduce((a, c) => a+c)}</p>)
   }
   
 

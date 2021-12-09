dbac9ce15b9d53e81e047775d0b268919fd4897d
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-12-09 12:19:43 +0200

    refactored middleware and logging to seperate files

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,17 +4,19 @@ const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
-// const middleware = require('./utils/middleware')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
 mongoose.connect(config.MONGODB_URI)
 
+app.use(middleware.requestLogger)
 app.use(cors())
 app.use(express.json())
 
 
 app.use('/api/blogs', blogsRouter)
 
-
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 module.exports = { app }

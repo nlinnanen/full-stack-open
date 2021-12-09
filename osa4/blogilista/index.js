ad2c5603fcfc3f13f03913970e6f91bda38083e8
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-12-09 11:37:31 +0200

    configure database and  enviroment variables

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,6 +4,8 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+const config = require('./config')
+
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
@@ -13,8 +15,8 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+
+mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
@@ -37,7 +39,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })

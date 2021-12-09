2c0bc85f27eee1f3c05f98f2489e9859534fdda2
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-12-09 12:14:23 +0200

    move routing to ./controllers

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,32 +1,20 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
-
+const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+// const middleware = require('./utils/middleware')
+
+const app = express()
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use('/api/blogs', blogsRouter)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = { app }

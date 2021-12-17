ae34e76633cb210bc2bbb08661709128e896d4b9
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-12-17 12:25:42 +0200

    refactor into async await

--- osa4/blogilista/tests/blogs_api.test.js
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -4,21 +4,18 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', () => {
-   api.get('/api/blogs', (request, response) => {
-    response
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-   })
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
 })
 
-test('get returns correct amount of blogs', () => {
-  api.get('/api/blogs', (request, response) => {
-   response
-     .expect(200)
-     .expect('Content-Type', /application\/json/)
-  })
-})
+// test('get returns correct amount of blogs', async () => {
+//   response = await api.get('/api/blogs')
+  
+//   expect(response.body).toHaveLength(0)
+// })
 
 afterAll(() => {
   mongoose.connection.close()

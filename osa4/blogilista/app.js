b98de6bf63eb4e523e741211c4157a53360ac376
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-12-17 10:48:36 +0200

    configure test db connection, fix request logging

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -10,10 +10,9 @@ const app = express()
 
 mongoose.connect(config.MONGODB_URI)
 
-app.use(middleware.requestLogger)
 app.use(cors())
 app.use(express.json())
-
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 

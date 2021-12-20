93e7ed3b59b8c4bdf087f4892fa1685700ca6896
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-12-20 15:31:33 +0200

    add user database schema

---
+++ osa4/blogilista/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  blogs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'blog'
+  }]
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+module.exports = mongoose.model('Blog', userSchema)

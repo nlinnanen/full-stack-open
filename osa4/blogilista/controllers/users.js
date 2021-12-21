0fbdb676a2cfc652b0b77ba7d796f077a3ebf570
Niklas Linnanen <niklas.linnanen@aalto.fi>
2021-12-21 13:20:07 +0200

    feat: get methods populate the user and blogs field

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -6,7 +6,14 @@ const User = require('../models/user')
 
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find({})
+    .populate('blogs', {
+      title: 1,
+      author: 1,
+      url: 1,
+      likes: 1
+    })
 
   if(users) {
     response.json(users)

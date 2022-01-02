996f2c5e80352ef1bc62c06ad80740048bba291d
Niklas Linnanen <niklas.linnanen@aalto.fi>
2022-01-02 21:32:54 +0200

    add button to view more info about a blog

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({blog}) => {
+  const [showInfo, setShowInfo] = useState(false)
+
+  if(showInfo) {
+    return (
+      <div className='blogInfo full'>
+        <div className='flex-space-apart'>
+          {blog.title}<button onClick={() => setShowInfo(false)}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div className='flex-space-apart'>
+          Likes &nbsp; {blog.likes} <button onClick={() => setShowInfo(false)}>Like</button>
+        </div>
+        <div>{blog.user.username}</div>
+      </div>
+    )
+  } else {
+    return (
+      <div className='blogInfo'>
+        <div className='flex-space-apart'>
+          {blog.title} {blog.author}
+          <button onClick={() => setShowInfo(true)}>view</button>
+        </div>
+      </div>  
+    )
+  }
+}
 
 export default Blog

3ee41857160c9be8f665370ecc3394580a0f8cee
Niklas Linnanen <niklas.linnanen@aalto.fi>
2022-01-02 22:01:14 +0200

    :D forgot a file like 3 commits ago

---
+++ osa5/bloglist-frontend/src/components/Togglable.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+const Togglable = (props) => {
+  const [show, setShow] = useState(false)
+
+  if(!show) {
+    return (
+      <button className="togglable-button" onClick={() => setShow(true)}>{props.buttonLabel}</button>
+    )
+  } else {
+    return (
+      <>
+        {props.children}
+        <button className="cancel-togglabe" onClick={() => setShow(false)}>Cancel</button>
+      </>
+    )
+  }
+}
+
+export default Togglable

0d2e3ca8a07be459cc94e49cebb81b5da76c09be
Niklas Linnanen <niklas.linnanen@aalto.fi>
2022-01-02 22:04:38 +0200

    Sort blogs by likes

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -65,9 +65,14 @@ const App = () => {
         <Togglable buttonLabel='Create new blog'>
           <BlogForm user={user} editMessage={editMessage} blogs={blogs} setBlogs={setBlogs}/>
         </Togglable>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} handleLike={handleLike}/>
-        )}
+        {blogs
+          .sort((a, b) => 
+            b.likes - a.likes
+          )
+          .map(blog =>
+            <Blog key={blog.id} blog={blog} handleLike={handleLike}/>
+          )
+        }
       </div>
     )
   }
